db91947bc889b8a09f04a0f3358bda7f80edb540
plbonnier <pl_bonnier@yahoo.fr>
2024-11-06 16:04:25 +0100

    fin 1ere partie du productshow, attente d'avoir gere plusieurs images pour continuer

--- assets/app.js
+++ assets/app.js
@@ -8,16 +8,29 @@ import './bootstrap.js';
  * which should already be in your base.html.twig.
  */
 
-const images = document.querySelectorAll('.card-product');
+document.addEventListener('DOMContentLoaded', () => {
+    const images = document.querySelectorAll('.card-product');
 
-// Ajoute des gestionnaires d'événements pour chaque image
-images.forEach(image => {
-    image.addEventListener('mouseover', function() {
-        image.style.transition = 'transform 0.3s ease-in-out';
-        image.style.transform = 'scale(1.1)'; // Agrandit l'image lors du survol
+    // Ajoute des gestionnaires d'événements pour chaque image
+    images.forEach(image => {
+        image.addEventListener('mouseover', function() {
+            image.style.transition = 'transform 0.3s ease-in-out';
+            image.style.transform = 'scale(1.1)'; // Agrandit l'image lors du survol
+        });
+
+        image.addEventListener('mouseout', function() {
+            image.style.transform = 'scale(1)'; // Réinitialise la taille de l'image lors de la sortie
+        });
     });
 
-    image.addEventListener('mouseout', function() {
-        image.style.transform = 'scale(1)'; // Réinitialise la taille de l'image lors de la sortie
+    // Ajoute un gestionnaire d'événements pour chaque image
+    const images2 = document.querySelectorAll('.images2');
+    const bigImage = document.querySelector('.big-image');
+
+    images2.forEach(image => {
+        image.addEventListener('click', function() {
+            const src = image.getAttribute('src');
+            bigImage.setAttribute('src', src);
+        });
     });
 });
